429d466bcc7dedae7413ec8c7e790f3591587ec2
Admin <admin@Admins-MacBook-Pro-2.local>
2019-04-01 10:52:20 +0100

    Distributor renamed Importer

--- Project_6B/contracts/coffeeaccesscontrol/ImporterRole.sol
+++ Project_6B/contracts/coffeeaccesscontrol/ImporterRole.sol
@@ -3,54 +3,54 @@ pragma solidity ^0.4.24;
 // Import the library 'Roles'
 import "./Roles.sol";
 
-// Define a contract 'DistributorRole' to manage this role - add, remove, check
-contract DistributorRole {
+// Define a contract 'ImporterRole' to manage this role - add, remove, check
+contract ImporterRole {
 
     using Roles for Roles.Role;
 
     // Define 2 events, one for Adding, and other for Removing
-    event DistributorAdded(address indexed account);
-    event DistributorRemoved(address indexed account);
+    event ImporterAdded(address indexed account);
+    event ImporterRemoved(address indexed account);
 
     // Define a struct 'distributors' by inheriting from 'Roles' library, struct Role
     Roles.Role private distributors;
 
     // In the constructor make the address that deploys this contract the 1st distributor
     constructor() public {
-        _addDistributor(msg.sender);
+        _addImporter(msg.sender);
     }
 
   // Define a modifier that checks to see if msg.sender has the appropriate role
-  modifier onlyDistributor() {
-    require(isDistributor(msg.sender));
+  modifier onlyImporter() {
+    require(isImporter(msg.sender));
     _;
   }
 
-  // Define a function 'isDistributor' to check this role
-  function isDistributor(address account) public view returns (bool) {
+  // Define a function 'isImporter' to check this role
+  function isImporter(address account) public view returns (bool) {
         return distributors.has(account);
   }
 
-  // Define a function 'addDistributor' that adds this role
-  function addDistributor(address account) public onlyDistributor {
-        _addDistributor(account);
+  // Define a function 'addImporter' that adds this role
+  function addImporter(address account) public onlyImporter {
+        _addImporter(account);
   }
 
-  // Define a function 'renounceDistributor' to renounce this role
-  function renounceDistributor() public {
-        _removeDistributor(msg.sender);
+  // Define a function 'renounceImporter' to renounce this role
+  function renounceImporter() public {
+        _removeImporter(msg.sender);
   }
 
-  // Define an internal function '_addDistributor' to add this role, called by 'addDistributor'
-  function _addDistributor(address account) internal {
+  // Define an internal function '_addImporter' to add this role, called by 'addImporter'
+  function _addImporter(address account) internal {
         distributors.add(account);
-        emit DistributorAdded(account);
+        emit ImporterAdded(account);
 
   }
 
-  // Define an internal function '_removeDistributor' to remove this role, called by 'removeDistributor'
-  function _removeDistributor(address account) internal {
+  // Define an internal function '_removeImporter' to remove this role, called by 'removeImporter'
+  function _removeImporter(address account) internal {
         distributors.remove(account);
-        emit DistributorRemoved(account);
+        emit ImporterRemoved(account);
   }
 }
